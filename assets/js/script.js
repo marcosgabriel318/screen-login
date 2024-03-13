a05695007d4e3116e9af0fd45f24e9950b77e205
Gabriel <marcosgabriel318@gmail.com>
2024-03-13 18:01:00 -0300

    Add funcionalidade de habilitar submit após input's preenchidos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,12 @@ const eyeicon = document.getElementById("eyeicon")
 const passwordInput = document.getElementById("password");
 const emailInput = document.getElementById("email");
 const submitBtn = document.querySelector("button");
-const divLogin = document.querySelector(".div-login");
+const submitForm = document.querySelector("form");
+const inputLogin = document.querySelectorAll(".input-login");
 
 
 const user_email= "example@email.com";
 const user_password = "randompassword";
-
-
-eyeicon.addEventListener('click', () => {
-    if(password.type == "password"){
-        password.type = "text";
-        eyeicon.src = "/assets/images/show.png"
-    } else {
-        password.type = "password"
-        eyeicon.src = "/assets/images/hide.png"
-    }
-})
 
 function displayLogged(){
     document.querySelector('.div-login').remove();
@@ -26,7 +16,7 @@ function displayLogged(){
 
 function displayIncorrectCredentials(){
     const element = document.createElement("span");
-    element.innerHTML = "Credentials Incorrect. Type again!"
+    element.innerHTML = "Incorrect Credentials. Type again!"
     element.classList.add("incorrect-credentials");
     divLogin.appendChild(element);
     setTimeout(()=>{
@@ -34,10 +24,41 @@ function displayIncorrectCredentials(){
     }, 3000)
 }
 
-submitBtn.addEventListener('click', ()=> {
+function hiddenPasswordDefault(){
+    password.type = "password";
+    eyeicon.src = "/assets/images/hide.png"
+}
+
+eyeicon.addEventListener('click', () => {
+    if(password.type == "password"){
+        password.type = "text";
+        eyeicon.src = "/assets/images/show.png"
+    } else {
+        hiddenPasswordDefault();
+    }
+})
+
+emailInput.addEventListener('input', () =>{
+    passwordInput.addEventListener('input', () => {
+        if(emailInput.value && passwordInput.value){
+            //submitBtn.removeAttribute("disabled");
+            submitBtn.classList.add('is-active');
+            submitBtn.disabled = false;
+        } else {
+            submitBtn.classList.remove('is-active')
+            submitBtn.disabled = true;
+        }
+    })
+})
+
+submitForm.addEventListener('submit', (event)=> {
     if (emailInput.value == user_email && passwordInput.value == user_password){
         displayLogged();
     } else {
         displayIncorrectCredentials();
+        event.preventDefault();
+        emailInput.value = ""
+        passwordInput.value = "";
+        hiddenPasswordDefault();
     }
 })
